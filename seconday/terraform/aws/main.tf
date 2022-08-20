7ae07570a6e9abb3ac1b3120ec7b039ec902c5c5
jfnandopr <jfnandopr@hotmail.com>
2022-08-20 18:58:34 -0300

    terraform

---
+++ seconday/terraform/aws/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.27"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_default_vpc" "default" {
+}
+
+resource "aws_security_group" "web" {
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description      = "HTTP Access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTPS Access"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "HTTP Access"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [ aws_security_group.web.id ]
+
+  tags = {
+    Name = "Aplicação Grails"
+  }
+}
