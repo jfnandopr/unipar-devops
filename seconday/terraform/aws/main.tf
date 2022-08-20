3a3fb073dc32e204a821a4a7dd7761070f4101ea
jfnandopr <jfnandopr@hotmail.com>
2022-08-20 19:15:14 -0300

    liberando porta 22

--- seconday/terraform/aws/main.tf
+++ seconday/terraform/aws/main.tf
@@ -40,6 +40,14 @@ resource "aws_security_group" "web" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "SSH Access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
